29ba43367ba177b0bfd8ad47b42d3a2218a2fe1c
DenisNysheta <nisetadenis@gmail.com>
2024-08-01 17:16:42 +0300

    add project to repository

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './normalize.css'
+import Practice from './components/Practice/Practice'
+import Redux from './components/Redux/Redux'
+import Clock from './components/Clock/Clock'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
