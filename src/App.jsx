56bfdb4e248bb54d7f21e5a3f0fd00360c9fc2a5
DenisNysheta <nisetadenis@gmail.com>
2024-08-01 17:27:12 +0300

    delete components that not need

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import './normalize.css'
-import Practice from './components/Practice/Practice'
-import Redux from './components/Redux/Redux'
-import Clock from './components/Clock/Clock'
 
 function App() {
 
