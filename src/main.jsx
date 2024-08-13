d6e263943df68d5024a4f17899590c63249bab70
Денис <161925751+DenisNysheta@users.noreply.github.com>
2024-08-13 11:01:44 +0300

    Add router and Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import store from './assets/scripts/store.js';
+
+import { Provider } from "react-redux"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   </React.StrictMode>,
 )
