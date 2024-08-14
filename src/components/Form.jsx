df7ef5a9bd212c0893fd33699f45002fc98905e4
DenisNysheta <nisetadenis@gmail.com>
2024-08-14 18:03:49 +0300

    Optimization animted letters and remove not need code lines

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,50 +1,41 @@
-import React, { useRef } from 'react'
-import "../assets/styles/components/Form.scss"
+import React, { useEffect, useRef, useState } from 'react'
+import "../assets/styles/components/Form/Form.scss"
+import InfoForm from './InfoForm.jsx/InfoForm'
+import animateLetters from '../assets/scripts/animateLetters'
 
 function Form() {
 
+  const [isAppearance, setAppearance] = useState(0)
+
+  let listLetters = []
+  for(let i = 0; i < 21; i++) {
+    let x = <span className='letter-title'></span>
+    listLetters.push(x)
+  }
+
+  useEffect(() => {
+    setTimeout(() => setAppearance(1),2800)
+  },[])
+
   return (
-    <div className='form'>
-        <h1 className='form__title'>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'> </span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-          <span className='letter-title'></span>
-        </h1>
-        <div className='form__info'>
-            <hgroup className='form__info__text'>
-              <h2 className='form__info__title'>
-                Lorem ipsum dolor sit amet.
-              </h2>
-              <p className='form__info__describe'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, facere, eveniet inventore quis aspernatur aliquam porro nostrum cum totam fugiat autem sed corrupti iste debitis!
-              </p>
-            </hgroup>
-            <div className='form__info__fields'>
-                <form className='form__info__form' method='get'>
-                  <input  type="text" placeholder='Email' />
-                  <input  type="text" placeholder='Password'/>
-                </form>
-            </div>
-        </div>
-    </div>
+    <>
+      <h1 className='form__title'>
+        {
+          listLetters.map( letter => letter)
+        }
+      </h1>
+      <div style={{opacity: isAppearance}}  className='form'>
+          <div className='form__info'>
+              <InfoForm />
+              <div className='form__info__fields'>
+                  <form className='form__info__form' method='get'>
+                    <input  type="text" placeholder='Email' />
+                    <input  type="text" placeholder='Password'/>
+                  </form>
+              </div>
+          </div>
+      </div>
+    </>
   )
 }
 
