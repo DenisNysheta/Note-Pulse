d683631b450bde41b6ae196229e66c120b253364
DenisNysheta <nisetadenis@gmail.com>
2024-08-15 21:00:55 +0300

    add letters animation for warning message

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -3,9 +3,29 @@ import Form from '../components/Form'
 import Settings from '../components/Settings/Settings'
 
 function Welcome() {
+
+  const mapObj = new Map()
+  let ivan = {name: "Ivan"}
+  mapObj.set(ivan, "dsadsadas")
+
+  console.log(mapObj.get(ivan))
+
+  let listLetters = []
+  for(let i = 0; i < 21; i++) {
+    let x = <span key={Date.now() + i + 5} className='letter-title'></span>
+    listLetters.push(x)
+  }
+
   return (
     <div className='pulse'>
-        <Form />
+        <div className='pulse__box'>
+          <h1 className='form__title'>
+            {
+              listLetters.map( letter => letter)
+            }
+          </h1>
+          <Form />
+        </div>
         <Settings />
     </div>
   )
