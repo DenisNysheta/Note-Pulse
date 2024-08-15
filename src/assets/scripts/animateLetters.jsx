187ba44a055d1f3686734cefdac93a3904896c43
DenisNysheta <nisetadenis@gmail.com>
2024-08-15 20:56:11 +0300

    add new parametrs for more stabilization

--- src/assets/scripts/animateLetters.jsx
+++ src/assets/scripts/animateLetters.jsx
@@ -1,8 +1,8 @@
-export default function(expression, classNameForLetter) {
+export default function(expression, classNameForLetter, animatenName, iteration, duration, delayAnima = 1) {
     let listLetters = []
     for(let i = 0; i < expression.length; i++) {
-        let delay = `1.${i}s`
-        let x = <span key={Date.now() + i + 4} style={{animationName: "greetings", animationDelay: delay, animationDuration: ".4s", animationIterationCount: 1, animationTimingFunction: "linear", animationFillMode: "forwards"}} className={classNameForLetter}>{expression[i]}</span>
+        let delay = `${delayAnima}.${i}s`
+        let x = <span key={Date.now() + i + 4} style={{animationName: animatenName, animationDelay: delay, animationDuration: duration, animationIterationCount: iteration, animationTimingFunction: "linear", animationFillMode: "forwards"}} className={classNameForLetter}>{expression[i]}</span>
         listLetters.push(x)
     }
     return listLetters
